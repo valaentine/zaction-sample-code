244bfe2bbb2a33acdfb1167927715974b3742f9a
GreateMacTouch <greatemactouch@GreateMacTouchui-MacBook-Pro.local>
2018-06-21 10:49:34 +0900

    send grid sample source

--- sendgrid/sendgrid.js
+++ sendgrid/sendgrid.js
@@ -1,13 +1,31 @@
 const sgMail = require('@sendgrid/mail');
-sgMail.setApiKey('API_KEY');
+sgMail.setApiKey("API KEY Here");
 
 module.exports = function (context, params) {
+    // const msg = {
+    //     to: 'gomanhera@naver.com',
+    //     from: 'rokakjw@sk.com',
+    //     subject: 'SendingTest01',
+    //     text: 'Success!!'
+    // };
     const msg = {
-        to: 'zaction@sk.com',
-        from: 'rokakjw@sk.com',
-        subject: 'Sending with SendGrid is Fun',
-        text: 'and easy to do anywhere, even with Node.js',
-        html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-    };
-    sgMail.send(msg);
+        to: params.to,
+        from: params.from,
+        subject: params.subject,
+        text: params.text
+    };    
+
+    return new Promise(function(resolve, reject){
+        sgMail.send(msg).then(arg => {
+            // console.log('email send successfully!');
+            // console.log(JSON.stringify(arg))
+            resolve({status:200, data: arg});
+        })
+        .catch(err => {
+            // console.log('email send error')
+            // console.log(JSON.stringify(err))
+            resolve({status:500, data: err});
+        })
+    })
+
 }
